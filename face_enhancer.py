c43b64500156cab85a10b5711b0fc973122e4681
G-force78 <114336644+G-force78@users.noreply.github.com>
2024-06-12 10:17:00 +0100

    Update face_enhancer.py
    fixed gpen
    
    Signed-off-by: G-force78 <114336644+G-force78@users.noreply.github.com>

--- face_enhancer.py
+++ face_enhancer.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import torch
 import gfpgan
+import upscaler.GPEN
 from PIL import Image
 from upscaler.RealESRGAN import RealESRGAN
 from upscaler.codeformer import CodeFormerEnhancer
@@ -30,8 +31,9 @@ def codeformer_runner(img, model):
 supported_enhancers = {
     "CodeFormer": ("./assets/pretrained_models/codeformer.onnx", codeformer_runner),
     "GFPGAN": ("./assets/pretrained_models/GFPGANv1.4.pth", gfpgan_runner),
-    "GPEN-BFR-512": ("./assets/pretrained_models/GPEN_BFR_512_pth", gpen_runner),
-    "GPEN-BFR-256": ("./assets/pretrained_models/GPEN_BFR_256_pth", gpen_runner),
+    #"GPEN-BFR-512": ("./assets/pretrained_models/GPEN_BFR_512_pth", gpen_runner),
+    "GPEN-BFR-512": ("./assets/pretrained_models/GPEN-BFR-512.onnx", gpen_runner),
+    "GPEN-BFR-256": ("./assets/pretrained_models/GPEN-BFR-256.onnx", gpen_runner),
     #"RestoreFormer": (dp.RESTOREFORMER_PATH, gpen_runner),
     "REAL-ESRGAN 2x": ("./assets/pretrained_models/RealESRGAN_x2.pth", realesrgan_runner),
     "REAL-ESRGAN 4x": ("./assets/pretrained_models/RealESRGAN_x4.pth", realesrgan_runner),
@@ -59,7 +61,7 @@ def load_face_enhancer_model(name='GFPGAN', device="cpu"):
     elif name == 'GFPGAN':
         model = gfpgan.GFPGANer(model_path=model_path, upscale=1, device=device)
     elif name.startswith('GPEN'):
-        model = GPEN(model_path=model_path, provider=provider, session_options=session_options)
+        model = GPEN(model_path=model_path, provider = ["CPUExecutionProvider"], session_options=None)
     elif name == "RestoreFormer":
         model = RestoreFormer(model_path=model_path, provider=provider, session_options=session_options)
     
